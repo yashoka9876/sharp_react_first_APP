9de6e1668658aa1504f431c16094dfa9efe0ed70
AshokYadav <yashoka51@gmail.com>
2023-12-10 00:52:01 +0530

    it contain variable......

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ function App() {
     <div >
       <h2>Let's get started</h2>
       <ExpenseItem />
-      <h1>Food Rs 10</h1>
-      <h1>Food Rs 100</h1>
-      <h1>Food Rs 200</h1>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,19 @@
+import './ExpenseItem.css'
 function ExpenseItem(){
-    return <h2> ExpenseItem!</h2>
-}
+    const expneseDate= new Date(2021,2,28);
+    const expenseTitle= 'Car Insurance';
+    const expenseAmount=294.67;
+    const LocationOfExpenditure ="delhi"
 
+    return(
+        <div className="expense-item">
+            <div>{expneseDate.toISOString()}</div>
+             <div className="expense-item__price">{LocationOfExpenditure}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
+            </div>
+        </div>
+    )
+}
 export default ExpenseItem
