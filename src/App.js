f985b1527676c30bdb30efff6491df7b580a9577
AshokYadav <yashoka51@gmail.com>
2023-12-11 01:10:25 +0530

    it conatain file structure and arrow funciton

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 import React from 'react';
 
-function App() {
+const App=()=> {
   const expenses=[
     {
       id:'e1',
